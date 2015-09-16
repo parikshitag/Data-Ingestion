1dad0d470c5dc3ffbc8513d3684324233792c9ae
Parikshit Agarwal <paagarwa@teksystems.com>
2015-09-16 18:32:51 +0530

    DDL script

--- src/com/teksystems/paagarwa/JsonReader.java
+++ src/com/teksystems/paagarwa/JsonReader.java
@@ -9,7 +9,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.UUID;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
@@ -17,11 +16,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 //You need to import the java.sql package to use JDBC
-import java.sql.*;
-
 //You need to import oracle.jdbc.driver.* in order to use the
 //API extensions.
-import oracle.jdbc.driver.*;
 
 
 public class JsonReader{
@@ -31,18 +27,27 @@ public class JsonReader{
 	int fType;
 	String[][] elements;
 	Connection conn ;
-	int num=2;
-	public JsonReader(String file) throws SQLException {
-		// Load the Oracle JDBC driver
+    
+
+    
+ public static void main(String[] args) throws SQLException {
+	// Load the Oracle JDBC driver
 	    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-	    // Connect to the database
-	    // You can put a database name after the @ sign in the connection URL.
-	    conn =
-	      DriverManager.getConnection ("jdbc:oracle:thin:@localhost:1521/exavm03", "wgp", "wgp");
-	    System.out.println("connection " + conn.isValid(10));
+
+	    String file = args[0];
+	    //System.out.println("filepath " + file);
 	    long start = System.nanoTime();
-	    ((OracleConnection)conn).setDefaultExecuteBatch(1);
-	    try {
+        try {
+            new JsonReader().getNames(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long end = System.nanoTime();
+        System.out.println("Time taken (nano seconds): " + (end - start));
+    }
+ void getNames(String file) throws Exception {
+
+     try {
 			InputStream in = new FileInputStream(file);
 			JsonParser jsonParser = jsonFactory.createParser(in);
 	        parseObject(jsonParser);
@@ -50,32 +55,19 @@ public class JsonReader{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    long end = System.nanoTime();
-        System.out.println("Time taken (nano seconds): " + (end - start));
-        conn.close();
-	}
-    
-
-
+ }
  public void parseObject(JsonParser json) throws JsonProcessingException, IOException, SQLException  {
      JsonFactory factory = new JsonFactory();
+
      ObjectMapper mapper = new ObjectMapper(factory);
      JsonNode rootNode = mapper.readTree(json);  
-     String ques="?";
-	 for(int i=0;i<30;i++)
-		 ques += ",?";
-     String ins = "INSERT INTO MASTER_META_DATA VALUES(" + ques + ")";
- 	 System.out.println(ins);
- 	 PreparedStatement ps =
-		      conn.prepareStatement (ins);
- 	 ps.setString(1, UUID.randomUUID().toString());
-     parseNode(rootNode,ps);
-     ps.close();
+     parseNode(rootNode,1);
  }
  
- public void  parseNode(JsonNode json,PreparedStatement ps) throws JsonProcessingException, IOException, SQLException  {
+ public void  parseNode(JsonNode json, int numberOfSpaces) throws JsonProcessingException, IOException, SQLException  {
      JsonNode rootNode = json;
      
+     String space = String.format("%"+ numberOfSpaces +"s", " ");
      Iterator<Map.Entry<String,JsonNode>> fieldsIterator = rootNode.fields();
      while (fieldsIterator.hasNext()) {
     	 
@@ -83,14 +75,14 @@ public class JsonReader{
          if(field.getValue().isObject()){	
         	 
         	 //System.out.println(field.getKey());
-        	 parseNode(field.getValue(),ps);
+        	 parseNode(field.getValue(),numberOfSpaces);
         	 //System.out.println("Total att: " + rec.size());
         	 
         	 
         	    //System.out.println ("Number of rows updated now: " + rows);  
          }else if(field.getValue().isArray()){
         	 /*String tblName = field.getKey();   
-        	 //System.out.println(tblName);
+        	 System.out.println(tblName);
         	 for (final JsonNode objNode : field.getValue()) {
         		 String ins = "INSERT INTO " + tblName + " VALUES(?,?,?,?,?,?,?,?,?,?)";
         		 
@@ -99,30 +91,21 @@ public class JsonReader{
             	 numberOfSpaces-=5;
                	    //System.out.println ("Number of rows updated now: " + rows);  
         		 break;
-        	 }
-        	 */
+        	 }*/
+        	 
          }else{	 
-        	 if(field.getValue().isInt() || field.getValue().isDouble()){
-        		 System.out.println(num + " " + field.getKey() +  " NUMBER, " + field.getValue());
-        		 ps.setInt(num++, field.getValue().asInt());
+        	 int size = field.getValue().asText().length() + 10;
+        	 if (field.getValue().isInt()){
+        		 System.out.println(field.getKey() + " NUMBER(" + size + "),");
+        	 }else if(field.getValue().isDouble()){
+        		 System.out.println(field.getKey() + " DOUBLE(" + size + "),");
         	 }else if(field.getValue().isBoolean()){
-        		 System.out.println(num + " bool " + field.getValue());
-        		 ps.setBoolean(num++, field.getValue().asBoolean());
-        	 }else{
-        		 System.out.println(num + " string " + field.getValue());
-        		 ps.setString(num++, field.getValue().asText());
-        	 }
+        		 System.out.println(field.getKey() + " NUMBER(1),");
+        	 }else {	 
+				System.out.println(field.getKey() + " VARCHAR2(" + size + "),");
+			}
          }
      }
  }
- 
- public static void main(String[] args) throws SQLException {
-     try {
-         new JsonReader(args[0]);
-     } catch (Exception e) {
-         e.printStackTrace();
-     }
-     
-}
 }
 
